d8bbe4cd4b06c00c1476b704fb0de5cb13ec456d
Manan-Arora31 <manan31102002@gmail.com>
2023-11-12 22:51:24 +0530

    Added Login/Signup Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import {BrowserRouter as Router,Route,Routes,Navigate } from 'react-router-dom';
 import AdminQuizCreation from './components/admin/AdminQuizCreation';
 import QuizList from './components/quiz/QuizList';
+import Main from "./components/Main";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
- 
+  const user = localStorage.getItem("token");
   return (
     <div>   
      <Router>
@@ -12,7 +15,10 @@ function App() {
           
            <Route path="/admin/create-quiz" element={<AdminQuizCreation />} />
             <Route path="/quizzes" element={ <QuizList />} />
-
+            {user && <Route path="/" exact element={<Main />} />}
+			      <Route path="/signup" exact element={<Signup />} />
+			      <Route path="/login" exact element={<Login />} />
+			      <Route path="/" element={<Navigate replace to="/login" />} />
 
             </Routes>
   
