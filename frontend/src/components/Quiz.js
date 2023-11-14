77c77c4b4b006270662f4d6fd09ab45cdb220da2
Manan-Arora31 <manan31102002@gmail.com>
2023-11-14 20:46:26 +0530

    Added timer feature

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -2,14 +2,51 @@ import React, { useEffect, useState } from 'react';
 import Questions from './Questions';
 import { useDispatch, useSelector } from 'react-redux';
 import {Navigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom"
 
 import { MoveNextQuestion, MovePrevQuestion } from '../hooks/FetchQuestions';
 import { PushAnswer } from '../hooks/setResult';
 
 
 function Quiz() {
+  const styles = {
+    color:"white"
+  }
+
+  const navigate = useNavigate();
 
   const [check,setChecked] = useState(undefined);
+
+  const [quizTime, setQuizTime] = useState(30); 
+  const [questionTime, setQuestionTime] = useState(10);
+
+  useEffect(() => {
+    const quizTimerInterval = setInterval(() => {
+      if (quizTime === 0) {
+        clearInterval(quizTimerInterval);
+        // alert('Time is up! Redirecting to the result page.');
+        // navigate('/result')
+        redirectToResultPage();
+      } else {
+        setQuizTime(quizTime - 1);
+      }
+    }, 1000);
+
+    const questionTimerInterval = setInterval(() => {
+      if (questionTime === 0) {
+        clearInterval(questionTimerInterval);
+        onNext();
+      } else {
+        setQuestionTime(questionTime - 1);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(quizTimerInterval);
+      clearInterval(questionTimerInterval);
+    };
+  }, [quizTime, questionTime]);
+
   
   const { queue , trace} = useSelector(state => state.questions);
   const {result} = useSelector(state => state.result);
@@ -19,9 +56,14 @@ function Quiz() {
     console.log(result);
   })
 
-  
+  const redirectToResultPage = ()=>{
+    alert('Time is up! Redirecting to the result page.');
+    navigate('/result')
+  }
+
 function onNext(){
   console.log('On next click');
+  setQuestionTime(10);
   if(trace < queue.length){
     dispatch(MoveNextQuestion());
     if(result.length <= trace)
@@ -50,6 +92,8 @@ if(result.length && result.length>=queue.length){
   return (
     <div className='container'>
         <h1 className='title text-light'>Quiz Application</h1>
+        <div style={styles} id="quiz-timer">Total Time: {Math.floor(quizTime / 60)}:{quizTime % 60}</div>
+        <div style={styles} id="question-timer">Question Time: {Math.floor(questionTime / 60)}:{questionTime % 60}</div>
 
         {/* display questions */}
         <Questions onChecked={onChecked}/>
