fde95594621326eb629eb44d43181c8619186d3b
Madhvendra Prasad <madhvendraprasad0123@gmail.com>
2023-11-18 00:57:51 +0530

    added categorywise quiz feature

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -33,7 +33,7 @@ const router=createBrowserRouter([
     element : <Main/>
   },
   {
-    path: '/quiz' ,
+    path: '/quiz/:id' ,
     element:<Quiz/>
   },
   {
--- frontend/src/components/MCQ.js
+++ frontend/src/components/MCQ.js
@@ -1,60 +1,62 @@
-import React, { useRef } from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function MCQ() {
-  const inputRef = useRef(null); 
-    const startQuiz=0;
-  return(
+  const [quizzes, setQuizzes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchQuizzes = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/quiz/reqQuizzes?category=MCQ');
+        console.log(response.data);
+        setQuizzes(response.data);
+      } catch (err) {
+        setError(err.message || 'An error occurred while fetching quizzes.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchQuizzes();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
     <div className='container'>
-        <h1 className='title text-light'>MCQ Quizes</h1>
+      <h1 className='title text-light'>MCQ Quizzes</h1>
 
-        <ul>
-            <li>You will be asked 10 questions one after another.</li>
-            <li>10 points is awarded for the correct answer.</li>
-            <li>Each question has three options. You can choose only one options.</li>
-            <li>You can review and change answers before the quiz finish.</li>
-            <li>The result will be declared at the end of the quiz.</li>
-        </ul>
+      <ul>
+        <li>You will be asked 10 questions one after another.</li>
+        <li>10 points are awarded for the correct answer.</li>
+        <li>Each question has three options. You can choose only one option.</li>
+        <li>You can review and change answers before the quiz finishes.</li>
+        <li>The result will be declared at the end of the quiz.</li>
+      </ul>
 
-        <div className='container2'>
-          <h2>Topics</h2>
-          <div className='topics'>
-          <Link to={'MCQ'}>
-            <div class="topic_of_quiz">
-              <h3>Mathematics</h3>
-            </div>
-          </Link>
-          <Link to={'MCQ'}>
-            <div class="topic_of_quiz">
-              <h3>Mathematics</h3>
-            </div>
-          </Link>
-          <Link to={'MCQ'}>
-            <div class="topic_of_quiz">
-              <h3>Mathematics</h3>
-            </div>
-          </Link>
-          <Link to={'MCQ'}>
-            <div class="topic_of_quiz">
-              <h3>Mathematics</h3>
-            </div>
-          </Link>
-          </div>
-            
+      <div className='container2'>
+        <h2>Topics</h2>
+        <div className='topics'>
+          {quizzes.map((data, i) => (
+            <Link to={`/quiz/${data._id}`} key={i}>
+              <div className="topic_of_quiz">
+                <h3>{data.title}</h3>
+              </div>
+            </Link>
+          ))}
         </div>
-
-        
-
-        {/* <form id="form">
-            <input ref={inputRef} className="userid" type="text" placeholder='Username*' />
-        </form>
-
-        <div className='start'>
-            <Link className='btn' to={'quiz'} onClick={startQuiz}>Start Quiz</Link>
-        </div> */}
-
+      </div>
     </div>
-  ) 
+  );
 }
 
-export default MCQ
+export default MCQ;
