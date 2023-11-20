1005be0d061632d178fb8d44af449f6503dae633
Pranil <praniltripathi7290@gmail.com>
2023-11-20 23:39:07 +0530

    added css to create quiz

--- frontend/src/components/admin/AdminQuizCreation.jsx
+++ frontend/src/components/admin/AdminQuizCreation.jsx
@@ -2,6 +2,7 @@ import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import Layout from './Layout';
 import { useNavigate } from 'react-router-dom';
+import '../../styles/AdminQuizCreation.css';
 const AdminQuizCreation = () => {
   const navigate=useNavigate();
   const [quizDetails, setQuizDetails] = useState({
