d2930c1ec1b0b08b5f075e08f122811b48c9cef2
Manan-Arora31 <manan31102002@gmail.com>
2023-11-25 10:30:32 +0530

    Added webcam

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useRef,useEffect, useState } from 'react';
 import Questions from './Questions';
 import { useDispatch, useSelector } from 'react-redux';
 import {Navigate} from 'react-router-dom'
@@ -10,6 +10,7 @@ import { PushAnswer } from '../hooks/setResult';
 import axios from 'axios';
 import '../styles/App.css'
 import '../styles/Quiz.css'
+import Webcam from 'react-webcam';
 
 
 function Quiz() {
@@ -30,6 +31,103 @@ function Quiz() {
     fontSize:"30px",
     
   }
+
+  // const WebcamCapture = () => {
+    const webcamRef = useRef(null);
+  
+    function base64ToImage(base64String) {
+      // Remove the data:image/png;base64, prefix
+      var base64Image = base64String.replace(/^data:image\/(png|jpeg);base64,/, '');
+    
+      // Decode the Base64 string
+      var binaryImg = atob(base64Image);
+    
+      // Create a typed array from the binary data
+      var length = binaryImg.length;
+      var arrayBuffer = new ArrayBuffer(length);
+      var uint8Array = new Uint8Array(arrayBuffer);
+      for (var i = 0; i < length; i++) {
+          uint8Array[i] = binaryImg.charCodeAt(i);
+      }
+    
+      // Create a Blob from the typed array
+      var blob = new Blob([uint8Array], { type: 'image/jpeg' }); // Adjust the type accordingly
+    
+      // Create an Image object
+      var img = new Image();
+    
+      // Set the image source to the Blob representing the decoded image
+      img.src = URL.createObjectURL(blob);
+    
+      return img;
+    }
+  
+  function base64ToBlob(base64) {
+    const byteCharacters = atob(base64.split(",")[1]);
+    const byteNumbers = new Array(byteCharacters.length);
+  
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+  
+    const byteArray = new Uint8Array(byteNumbers);
+    return new Blob([byteArray], { type: "image/png" }); // Adjust the type based on your image format
+  }
+  
+  
+    useEffect(() => {
+      const enableWebcam = async () => {
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+          const tracks = stream.getTracks();
+  
+          if (webcamRef.current) {
+            webcamRef.current.video.srcObject = stream;
+          }
+  
+          // Cleanup: Stop tracks when component unmounts
+          return () => tracks.forEach(track => track.stop());
+        } catch (error) {
+          console.error('Error accessing webcam:', error);
+        }
+      };
+  
+      enableWebcam();
+    }, []); // Run once on component mount
+  
+  var image;
+  let screenshot;
+  const [imageSrc, setImageSrc] = useState('');
+  
+  const captureAndUploadPhoto = async () => {
+    if (webcamRef.current) {
+      screenshot = webcamRef.current.getScreenshot();
+      
+      // const image = base64ToImage(screenshot);
+      const image = base64ToBlob(screenshot);
+      console.log(image);
+      setImageSrc(image.src);
+  
+      const formData = new FormData();
+      formData.append("image",image,"screenshot.png");
+      
+      const result = await axios.post(
+        "http://localhost:8000/upload",
+        formData,
+        {
+          headers : {"Content-Type": "multipart/form-data"},
+        }
+      );
+      console.log(result.data);
+    }
+  }
+  
+  useEffect(() => {
+      const intervalId = setInterval(captureAndUploadPhoto, 5000);
+  
+      return () => clearInterval(intervalId); // Cleanup on component unmount
+    }, [captureAndUploadPhoto]);
+// }
 
   const navigate = useNavigate();
 
@@ -121,6 +219,7 @@ if(result.length && result.length>=queue.length){
 }
 
   return (
+    <div className='webdiv'>
     <div className='container'>
         <h1 className='title text-light'>Quiz Application</h1>
         <div className='clocks'>
@@ -136,6 +235,16 @@ if(result.length && result.length>=queue.length){
             {trace>0?<button className='btn prev' onClick={onPrev}>Prev</button>:<div></div>}
             <button className='btn next' onClick={onNext}>Next</button>
         </div>
+    </div>
+    <div>
+      <Webcam
+        className = 'webcam'
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/png"
+        videoConstraints={{ facingMode: 'user' }}
+      />
+    </div>
     </div>
   )
 }
